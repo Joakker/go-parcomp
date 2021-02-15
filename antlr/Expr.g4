68b144ea548c763a39487b9e15d78925b65b9637
Joakker <joaquinandresleon108@gmail.com>
2021-02-14 21:46:40 -0300

    Comment in all functions in interpreter.go and all rules in Expr.g4

--- antlr/Expr.g4
+++ antlr/Expr.g4
@@ -1,20 +1,32 @@
 grammar Expr;
 
-file: stmt* ;
+file: stmt* ;   // A file consists of many statements
 stmt: expr                      # ExprStmt
+        // If a statement corresponds to an expression, print it's result
     | namelist '=' exprlist     # AssignStmt
+        // Assign exprlist to namelist
     ;
+
+// The order in which options are defined denotes the precedence they have
+// compared to the others
 expr: '(' expr ')'                  # NoOp
+        // Parentheses have the highest precedence
     | <assoc=right> expr '^' expr   # Pow
+        // Exponents go next
     | expr op=('*' | '/') expr      # MulDiv
+        // Multiplication and division have the same precedence
     | expr op=('+' | '-') expr      # AddSub
+        // Ditto with addition and subtraction
     | NUM                           # Number
     | ID                            # Identifier
+        // Finally, numbers and identifiers
     ;
 
-namelist: ID (',' ID)*      ;
-exprlist: expr (',' expr)*  ;
+namelist: ID (',' ID)*      ;   // Define this so we can use GetAllID() in the code
+exprlist: expr (',' expr)*  ;   // This one isn't necessary but for the sake of
+                                // consistency
 
+// Define these so we can easily refer to them by name in the code
 POW : '^' ;
 MUL : '*' ;
 DIV : '/' ;
@@ -29,5 +41,7 @@ ID  : ID_START ID_CONTINUE*         ;
 NUM : [0-9]+ ('.' [0-9]+)?          ;
 WS  : [\p{White-Space}]+    -> skip ;
 
+// Use \p{Something} to select characters inside range Something or that have a
+// the corresponding attribute
 fragment ID_START       : [\p{Alpha}\p{General-Category=Other-Letter}]  ;
 fragment ID_CONTINUE    : [\p{Alnum}\p{General-Category=Other-Letter}]  ;
--- antlr/interpreter.go
+++ antlr/interpreter.go
@@ -9,12 +9,16 @@ import (
 	"github.com/Joakker/go-parcomp/antlr/parser"
 )
 
+// Interpreter listens to events in the parse tree and carries the logic for
+// defining variables and calculating expressions
 type Interpreter struct {
 	parser.BaseExprListener
-	Variable map[string]float64
-	Stack    []float64
+	Variable map[string]float64 // Contains all the named variables and their values
+	Stack    []float64          // Contains values while an expression is being calculated
 }
 
+// EnterNumber calculates the float64 value of the number and pushes
+// it onto the Stack
 func (i *Interpreter) EnterNumber(ctx *parser.NumberContext) {
 	val, err := strconv.ParseFloat(ctx.GetText(), 64)
 	if err != nil {
@@ -23,6 +27,8 @@ func (i *Interpreter) EnterNumber(ctx *parser.NumberContext) {
 	i.Push(val)
 }
 
+// EnterIdentifier retrieves the value associated with the identifier, or
+// math.NaN() if it doesn't exist, and pushes it onto the Stack
 func (i *Interpreter) EnterIdentifier(ctx *parser.IdentifierContext) {
 	val, ok := i.Variable[ctx.GetText()]
 	if !ok {
@@ -31,6 +37,10 @@ func (i *Interpreter) EnterIdentifier(ctx *parser.IdentifierContext) {
 	i.Push(val)
 }
 
+// ExitMulDiv groups the operands '*' and '/' together, applying the
+// corresponding operation to the 2 topmost values on the Stack. Since we have
+// to have visited both of them before doing this action, it uses `Exit' instead
+// of `Enter'.
 func (i *Interpreter) ExitMulDiv(ctx *parser.MulDivContext) {
 	b := i.Pop()
 	a := i.Pop()
@@ -46,6 +56,10 @@ func (i *Interpreter) ExitMulDiv(ctx *parser.MulDivContext) {
 	}
 }
 
+// ExitAddSub groups the operands '+' and '-' together, applying the
+// corresponding operation to the 2 topmost values on the Stack. Since we have
+// to have visited both of them before doing this action, it uses `Exit' instead
+// of `Enter'.
 func (i *Interpreter) ExitAddSub(ctx *parser.AddSubContext) {
 	b := i.Pop()
 	a := i.Pop()
@@ -58,6 +72,9 @@ func (i *Interpreter) ExitAddSub(ctx *parser.AddSubContext) {
 	}
 }
 
+// ExitAssignStmt defines all the names in the left hand side and assigns the
+// expressions in the right to them in order. The length of both lists must be
+// the same, otherwise no variable will be assigned
 func (i *Interpreter) ExitAssignStmt(ctx *parser.AssignStmtContext) {
 	name := ctx.Namelist().(*parser.NamelistContext).AllID()
 	if len(name) != len(i.Stack) {
@@ -69,14 +86,18 @@ func (i *Interpreter) ExitAssignStmt(ctx *parser.AssignStmtContext) {
 	}
 }
 
+// ExitExprStmt prints the resulting value of a statement when it consists of an
+// expression
 func (i *Interpreter) ExitExprStmt(ctx *parser.ExprStmtContext) {
 	fmt.Println(i.Pop())
 }
 
+// Push appends a value val to the Stack
 func (i *Interpreter) Push(val float64) {
 	i.Stack = append(i.Stack, val)
 }
 
+// Pop deletes the topmost value off the Stack and returns it
 func (i *Interpreter) Pop() float64 {
 	val := i.Stack[len(i.Stack)-1]
 	i.Stack = i.Stack[:len(i.Stack)-1]
